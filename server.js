0803b66c8a1073b9175c8b4c85f37363ce003a31
Yavi Creatives <yavicreatives2026@gmail.com>
2026-05-27 00:02:35 +0530

    fix: correct double spacing in server startup log message

--- server.js
+++ server.js
@@ -43,5 +43,5 @@ app.use('/', require('./src/routes/adminRoutes'));
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server  is running on port ${PORT}`);
 });
